3fc30aabaf4351d3c0fe5a31019616b1ad791da2
Mark Goodhead <goodhead@gmail.com>
2023-06-16 12:42:35 +0100

    Create ctypes.py

---
+++ ctypes.py
@@ -0,0 +1,11 @@
+import ctypes
+import ctypes.util
+
+name = ctypes.util.find_library("opengl32")
+if name is None:
+    print("Library not found.")
+else:
+    lib = ctypes.CDLL(name)
+    path = ctypes.create_unicode_buffer(1024)  # adjust buffer size if necessary
+    ctypes.windll.kernel32.GetModuleFileNameW(lib._handle, path, len(path))
+    print(path.value)
